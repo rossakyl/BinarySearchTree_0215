c259336dd90995980fe37d3e3a9b4f96fc5d572f
rossakyl <rossakayla01@gmail.com>
2025-06-24 10:52:33 +0700

    Search for insertion point using search() function

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -42,6 +42,11 @@ public:
         newNode->leftchild = nullptr;
         newNode->rightchild = nullptr;
 
+        // Step 4: Locate the Node which will be the parent of the node to be inserted
+        Node *parent = nullptr;
+        Node *currentNode = nullptr;
+        search(x, parent, currentNode);
+
         
     }
     

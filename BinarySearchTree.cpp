1d0730b45a0f20b465b198f4324bef488cbeab01
rossakyl <rossakayla01@gmail.com>
2025-06-24 10:48:15 +0700

    Add Node class constructor initializing children to nullptr

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -8,4 +8,11 @@ public:
     Node *leftchild;
     Node *rightchild;
 
+    // Constructor for the node class
+    Node()
+    {
+        leftchild = nullptr;  // intialize left child to null
+        rightchild = nullptr; // intialize right child to null
+    }
 };
+
